b6a0537e698cfa38dd977cebadfbac987ff88865
Bladish <Bladish@users.noreply.github.com>
2018-10-23 10:34:16 +0200

    Lets begin

---
+++ GameOfLifeUnity/GameOfLife/Assets/Cell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    public bool alive;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetState(bool alive)
+    {
+        if (alive)
+        {
+            GetComponent<MeshRenderer>().enabled = true; 
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
---
+++ GameOfLifeUnity/GameOfLife/Assets/GameOfLife.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOfLife : MonoBehaviour {
+
+    public GameObject cellPrefab;
+    public float tick;
+    float nextTick = 0;
+    GameObject[] cells;
+	// Use this for initialization
+	void Start () {
+        cells = new GameObject[10];
+
+        for(int i = 0; i < cells.Length; i++)
+        {
+            Vector3 spawnOffset = new Vector3(i, 0, 0);
+            cells[i] = Instantiate(cellPrefab, transform.position + spawnOffset, transform.rotation, transform);
+            if(Random.Range(0, 100) > 10)
+            {
+                cells[i].GetComponent<Cell>().SetState(false);
+            }
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(nextTick > tick)
+        {
+            nextTick -= tick;
+        }
+
+        nextTick += Time.deltaTime;
+	}
+}
